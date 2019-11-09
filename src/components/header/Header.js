2a8cc59e332d76f208993beb7a2690bc1e7cb4d9
wouter <wkurvers11@gmail.com>
2019-11-09 13:29:36 +0100

    updated header with proper bootstrap

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import 'antd/dist/antd.css';
 import './Header.css'
-import {Nav, Navbar} from 'react-bootstrap'
+import {Nav, Navbar, NavDropdown, NavItem,} from 'react-bootstrap'
 
 
 class Header extends Component {
@@ -10,11 +10,19 @@ class Header extends Component {
             <div>
                 <div className={'headerMain'}>
                     <Navbar>
-                        <Navbar.Brand href="/">Navbar</Navbar.Brand>
+                        <Navbar.Brand href="/">MOI</Navbar.Brand>
                         <Nav className="mr-auto">
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="results">Results</Nav.Link>
                             <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+
+                        <Nav className="justify-content-end" activeKey="/home">
+                            <NavDropdown title="Mijn account" id="nav-dropdown">
+                                <NavDropdown.Item eventKey="4.1">mijn account</NavDropdown.Item>
+                                <NavDropdown.Divider/>
+                                <NavDropdown.Item eventKey="4.2">favorieten</NavDropdown.Item>
+                            </NavDropdown>
                         </Nav>
                     </Navbar>
                 </div>
