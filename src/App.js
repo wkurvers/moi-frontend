8dee5a649143135ef3d427f828f7d241ea92dfec
Gaauwe <gromdroid@gmail.com>
2019-11-12 13:41:35 +0100

    Finished header?

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import {Provider} from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 import Users from "./components/Users";
+import Header from "./components/Header/Header";
 
 class App extends Component {
   render() {
     return (
-        <Provider store={store}>
-          <div className="App">
-            <h1>Test</h1>
-              <Users/>
-          </div>
-        </Provider>
-
+      <Provider store={store}>
+        <div className="App">
+          <Header />
+          <h1>Test</h1>
+          <Users />
+        </div>
+      </Provider>
     );
   }
 }
