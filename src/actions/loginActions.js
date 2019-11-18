7d12bb204e99a560dadff590343b0b1e97b38666
Jelmer <jelmer.internet@xs4all.nl>
2019-11-18 15:35:46 +0100

    login with redux

--- src/actions/loginActions.js
+++ src/actions/loginActions.js
@@ -1,4 +1,4 @@
-import {LOGIN} from "./types";
+import {LOGIN, RESET_LOGIN_RESPONSE} from "./types";
 
 export const loginUser = (loginData) => dispatch =>  {
     fetch('http://localhost:8000/api/token/', {
@@ -8,11 +8,27 @@ export const loginUser = (loginData) => dispatch =>  {
         },
         body: JSON.stringify(loginData)
     })
-        .then(res => res.json())
-        .then(data =>  dispatch({
+    .then(res => res.json()
+    .then(data => {
+        data.status = res.status
+        dispatch({
             type: LOGIN,
             payload: data,
-        }))
+        })
+    }).catch(data => {
+        data.status = res.status
+        dispatch({
+            type: LOGIN,
+            payload: data,
+        })
+    })
+    )
 };
 
+
+export const resetResponse = () => dispatch => {
+    dispatch({
+        type: RESET_LOGIN_RESPONSE,
+    })
+};
 
