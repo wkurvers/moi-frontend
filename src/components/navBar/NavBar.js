5d21145199e4504eb9c5ecd9766e84a1f599e1ef
Gaauwe <gromdroid@gmail.com>
2019-11-24 22:21:01 +0100

    Updated navbar with responsive design

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "antd/dist/antd.css";
 import "./NavBar.css";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Avatar } from "antd";
 import AccountModal from "../../components/accountModal/AccountModal.js";
 
@@ -24,7 +24,7 @@ class NavBar extends Component {
       <div>
         <AccountModal ref={this.accountModal} />
 
-        <div className={"headerMain"}>
+        {/* <div className={"headerMain"}>
           <Navbar>
             <Navbar.Brand href="/">
               <img
@@ -51,7 +51,36 @@ class NavBar extends Component {
               />
             </Nav>
           </Navbar>
-        </div>
+        </div> */}
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            <img
+              style={{ display: "inline-block", marginRight: 10, marginTop: 3 }}
+              src={require("../../assets/MOI_logo.png")}
+              width="60"
+              height="40"
+              className="d-inline align-top"
+              alt="React Bootstrap logo"
+            />
+            MOI
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto"></Nav>
+            <Nav>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="results">Zoekprofiel</Nav.Link>
+              <Nav className="justify-content-end" activeKey="/home">
+                <Avatar
+                  size={42}
+                  style={{ cursor: "pointer" }}
+                  src="https://www.innovatiespotter.nl/assets/img/team/team-bob.jpg"
+                  onClick={this.showModal}
+                />
+              </Nav>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
