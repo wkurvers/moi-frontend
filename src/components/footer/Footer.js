dd006e359a0da4b9a0b543b44b453e5bda7cc83a
Gaauwe <gromdroid@gmail.com>
2019-11-24 23:07:21 +0100

    Started working on footer responsive design

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,10 +6,10 @@ import { Row, Col, Icon } from "antd";
 export default class Footer extends Component {
   render() {
     return (
-      <div className="footer">
-        <div className={"footer-image"} />
+      <Row className="footer">
+        <img className={"footer-image"} src={require("../../assets/connected_dots.png")} />
 
-        <div className="footer-title-container">
+        <Row className="footer-title-container">
           <h1 className="footer-title">
             Proin scelerisque{" "}
             <p className="footer-title" style={{ fontWeight: 400, display: "inline-block" }}>
@@ -21,28 +21,28 @@ export default class Footer extends Component {
             Nam tempus metus vitae vehicula ultricies. Etiam a quam purus. Nullam ornare vel lectus at scelerisque. Donec at libero commodo, accumsan
             neque nec, accumsan ligula.{" "}
           </h3>
-        </div>
-        <Row style={{ width: "45vw", marginTop: "5vh" }}>
-          <Col span={8}>
+        </Row>
+        <Row style={{ marginTop: "5vh" }} gutter={[48, 48]}>
+          <Col lg={8} md={8} xs={24}>
             <Icon style={{ fontSize: 36, color: "white", textAlign: "center", width: "100%", marginBottom: 10 }} type="shopping" />
 
             <p className={"footer-column-title"}>1,2 miljoen</p>
             <p className={"footer-column-subtitle"}>Bedrijven aangesloten</p>
           </Col>
-          <Col span={8}>
+          <Col lg={8} md={8} xs={24}>
             <Icon style={{ fontSize: 36, color: "white", textAlign: "center", width: "100%", marginBottom: 10 }} type="clock-circle" />
 
             <p className={"footer-column-title"}>24 uur</p>
             <p className={"footer-column-subtitle"}>Altijd up-to-date</p>
           </Col>
-          <Col span={8}>
+          <Col lg={8} md={8} xs={24}>
             <Icon style={{ fontSize: 36, color: "white", textAlign: "center", width: "100%", marginBottom: 10, fontWeight: 400 }} type="filter" />
 
             <p className={"footer-column-title"}>8 categorieën</p>
             <p className={"footer-column-subtitle"}>Duidelijk ingedeeld</p>
           </Col>
         </Row>
-      </div>
+      </Row>
     );
   }
 }
