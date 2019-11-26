c756fe4f6434dbbcf27b3572f22a667ac25f6247
jelmer <jelmer.internet@xs4all.nl>
2019-11-26 12:34:27 +0100

    lawform screen

--- src/components/searchProfileCreationModal/LawFormScreen.js
+++ src/components/searchProfileCreationModal/LawFormScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Row, Col} from 'antd';
+import {Row, Col, Button} from 'antd';
 import "./SearchProfileCreationModal.css"
 
 class LawFormScreen extends Component {
@@ -7,7 +7,37 @@ class LawFormScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      forms: [
+        'B.V.',
+        'C.V.',
+        'V.O.F.',
+        'Eenmanszaak',
+        'Coöperatie i.o.'
+      ],
+
+      activeForms: [],
+
     }
+  }
+
+  onClick(e) {
+    document.activeElement.blur() 
+    let activeForms = this.state.activeForms
+    if(!this.isActive(e.target.id)) {
+      activeForms.push(e.target.id)
+    } else {
+      activeForms.splice(activeForms.indexOf(e.target.id),1)
+    }
+    this.setState({activeForms: activeForms});
+  }
+
+  isActive(form) {
+    for(let index in this.state.activeForms) {
+      if(form == this.state.activeForms[index]) {
+        return true
+      }
+    }
+    return false
   }
 
   render() {
@@ -18,21 +48,48 @@ class LawFormScreen extends Component {
             Geef aan in welke rechtsvorm de bedrijven mogen hebben
           </div>
           <div className={"lawform-select-container"}>
-            <Row type="flex" justify="space-between" gutter={[16, 16]}>
-              <Col span={3}>
-                B.V.
+            <Row type="flex" justify="space-between" gutter={[8,8]}>
+              <Col>
+                {!this.isActive(this.state.forms[0]) &&
+                  <Button id={this.state.forms[0]} className={'lawform-container'} onClick={(e) => this.onClick(e)}>{this.state.forms[0]}</Button>
+                }
+                {this.isActive(this.state.forms[0]) &&
+                  <Button id={this.state.forms[0]} className={'lawform-container-active'} onClick={(e) => this.onClick(e)}>{this.state.forms[0]}</Button>
+                }
               </Col>
-              <Col span={3}>
-                C.V.
+              <Col>
+                {!this.isActive(this.state.forms[1]) &&
+                  <Button id={this.state.forms[1]} className={'lawform-container'} onClick={(e) => this.onClick(e)}>{this.state.forms[1]}</Button>
+                }
+                {this.isActive(this.state.forms[1]) &&
+                  <Button id={this.state.forms[1]} className={'lawform-container-active'} onClick={(e) => this.onClick(e)}>{this.state.forms[1]}</Button>
+                }
               </Col>
-              <Col span={3}>
-                V.O.F.
+              <Col>
+                {!this.isActive(this.state.forms[2]) &&
+                  <Button id={this.state.forms[2]} className={'lawform-container'} onClick={(e) => this.onClick(e)}>{this.state.forms[2]}</Button>
+                }
+                {this.isActive(this.state.forms[2]) &&
+                  <Button id={this.state.forms[2]} className={'lawform-container-active'} onClick={(e) => this.onClick(e)}>{this.state.forms[2]}</Button>
+                }
               </Col>
-              <Col span={3}>
-                Eenmanszaak
+            </Row>
+            <Row type="flex" justify="space-around" gutter={[8,8]}>
+              <Col>
+                {!this.isActive(this.state.forms[3]) &&
+                  <Button id={this.state.forms[3]} className={'lawform-container'} onClick={(e) => this.onClick(e)}>{this.state.forms[3]}</Button>
+                }
+                {this.isActive(this.state.forms[3]) &&
+                  <Button id={this.state.forms[3]} className={'lawform-container-active'} onClick={(e) => this.onClick(e)}>{this.state.forms[3]}</Button>
+                }
               </Col>
-              <Col span={3}>
-                Coöperatie i.o.
+              <Col>
+                {!this.isActive(this.state.forms[4]) &&
+                  <Button id={this.state.forms[4]} className={'lawform-container'} onClick={(e) => this.onClick(e)}>{this.state.forms[4]}</Button>
+                }
+                {this.isActive(this.state.forms[4]) &&
+                  <Button id={this.state.forms[4]} className={'lawform-container-active'} onClick={(e) => this.onClick(e)}>{this.state.forms[4]}</Button>
+                }
               </Col>
             </Row>
           </div>
