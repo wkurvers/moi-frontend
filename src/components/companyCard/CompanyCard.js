9bd4a95c8baccdba79cf51a275d99dd414e5bd7b
Gaauwe <gromdroid@gmail.com>
2019-11-27 11:24:22 +0100

    Dynamic data

--- src/components/companyCard/CompanyCard.js
+++ src/components/companyCard/CompanyCard.js
@@ -4,6 +4,10 @@ import "./CompanyCard.css";
 import themes from "../../utils/themes";
 
 export default class CompanyCard extends Component {
+  componentDidMount() {
+    console.log(Object.keys(this.props.company["Thema's"])[0].slice(3));
+  }
+
   render() {
     return (
       <Col className={"companycard-container"} lg={6} md={8} xs={24}>
@@ -11,11 +15,15 @@ export default class CompanyCard extends Component {
           className={"companycard-inner"}
           style={{
             background:
-              "linear-gradient(135deg, " + themes[this.props.company.theme].colorOne + " 0%, " + themes[this.props.company.theme].colorTwo + " 100%)"
+              "linear-gradient(135deg, " +
+              themes[Object.keys(this.props.company["Thema's"])[0].slice(9)].colorOne +
+              " 0%, " +
+              themes[Object.keys(this.props.company["Thema's"])[0].slice(9)].colorTwo +
+              " 100%)"
           }}
         >
-          <p className={"companycard-type"}>{this.props.company.type}</p>
-          <h5 className={"companycard-name"}>{this.props.company.name}</h5>
+          <p className={"companycard-type"}>{Object.values(this.props.company["Thema's"])[0]}</p>
+          <h5 className={"companycard-name"}>{this.props.company.Bedrijfsnaam}</h5>
           <hr />
         </div>
       </Col>
